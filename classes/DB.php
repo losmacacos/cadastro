c183a3208a559b78ab8d93ef98ba66c6f7f4ea76
losmacacos <alexandrferreira@gmail.com>
2017-11-12 14:15:32 -0200

    eleveth commit

--- classes/DB.php
+++ classes/DB.php
@@ -21,6 +21,7 @@ class DB{
 				                   'pdo.password' => $dbopts["pass"]
 				               )
 				);
+				$instance = $dbopts;
 
 				//self::$instance = new PDO('pgsql:host=ec2-107-22-167-179.compute-1.amazonaws.com;port=5432;dbname=dbqc4gqll2o9ps;user=wolqehbjdhqouv;password=6f8c7b8b99faa31823c88f37df3b6bc315f2320ee9cf75a033740c4c859f5322');
 				//self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
