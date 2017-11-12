4e7bcfd4526f53360f91f3b76fcdddadc324ffbd
losmacacos <alexandrferreira@gmail.com>
2017-11-12 12:58:02 -0200

    seventh commit

--- classes/DB.php
+++ classes/DB.php
@@ -11,7 +11,8 @@ class DB{
 		if(!isset(self::$instance)){
 
 			try {
-				self::$instance = new PDO('pgsql:host='.DB_HOST.';port=5432;dbname='.DB_NAME.';user='.DB_USER'.;password='DB_PASS);
+				self::$instance = new PDO('pgsql:host=ec2-107-22-167-179.compute-1.amazonaws.com;port=5432;dbname=dbqc4gqll2o9ps;user=wolqehbjdhqouv;password=6f8c7b8b99faa31823c88f37df3b6bc315f2320ee9cf75a033740c4c859f5322');
+				//self::$instance = new PDO('pgsql:host=' . ';port=5432'. DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 				self::$instance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 			} catch (PDOException $e) {
