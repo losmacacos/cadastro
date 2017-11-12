1f040bf1d3d2267c386d2c301f3c2bb298157de9
losmacacos <alexandrferreira@gmail.com>
2017-11-12 12:45:02 -0200

    fourth commit

--- classes/config.php
+++ classes/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'ec2-184-73-247-240.compute-1.amazonaws.com');
-define('DB_NAME', 'd2d426mobmp3vr');
-define('DB_USER', 'isrkneobqzzhwa');
-define('DB_PASS', '97b5e1448864a5814b0e5989abfe1e3ae46500e23e3da88ac8f4f89943ada075');
+define('DB_HOST', 'ec2-107-22-167-179.compute-1.amazonaws.com');
+define('DB_NAME', 'dbqc4gqll2o9ps');
+define('DB_USER', 'wolqehbjdhqouv');
+define('DB_PASS', '6f8c7b8b99faa31823c88f37df3b6bc315f2320ee9cf75a033740c4c859f5322');
 
 // define('DB_HOST', 'localhost');
 // define('DB_NAME', 'phpoo');
