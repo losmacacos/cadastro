f575df6bb55f2f5025a25da27110bed8790c8504
losmacacos <alexandrferreira@gmail.com>
2017-11-12 12:31:17 -0200

    second commit

--- classes/config.php
+++ classes/config.php
@@ -1,7 +1,12 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'phpoo');
-define('DB_USER', 'alexandreFerreira');
-define('DB_PASS', 'patriotspatriots');
+define('DB_HOST', 'ec2-184-73-247-240.compute-1.amazonaws.com');
+define('DB_NAME', 'd2d426mobmp3vr');
+define('DB_USER', 'isrkneobqzzhwa');
+define('DB_PASS', '97b5e1448864a5814b0e5989abfe1e3ae46500e23e3da88ac8f4f89943ada075');
+
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'phpoo');
+// define('DB_USER', 'alexandreFerreira');
+// define('DB_PASS', 'patriotspatriots');
 
