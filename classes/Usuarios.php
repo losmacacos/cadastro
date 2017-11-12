449bf8a1c930a7e534488159d19ce65873843418
losmacacos <alexandrferreira@gmail.com>
2017-11-12 16:51:32 -0200

    twenth-eight commit

--- classes/Usuarios.php
+++ classes/Usuarios.php
@@ -22,12 +22,9 @@ class Usuarios extends Crud{
 	private $sexo;
 	private $modalidade;
 
-
-
-
 	public function insert(){
 
-		$sql  = "INSERT INTO $this->table (nome, email, senha, endereco, numero, bairro, cidade, estado, cep, nomeMae, nomePai, numRG, numCPF, dataNasc, sexo, modalidade) VALUES (:nome, :email, :senha, :endereco, :numero, :bairro, :cidade, :estado, :cep, :nomeMae, :nomePai, :numRG, :numCPF, :dataNasc, :sexo, :modalidade)";
+		$sql  = "INSERT INTO usuarios (nome, email, senha, endereco, numero, bairro, cidade, estado, cep, nomeMae, nomePai, numRG, numCPF, dataNasc, sexo, modalidade) VALUES (:nome, :email, :senha, :endereco, :numero, :bairro, :cidade, :estado, :cep, :nomeMae, :nomePai, :numRG, :numCPF, :dataNasc, :sexo, :modalidade)";
 		$stmt = $this->prepare($sql);
 		$stmt->bindParam(':nome', $this->nome);
 		$stmt->bindParam(':email', $this->email);
@@ -51,7 +48,7 @@ class Usuarios extends Crud{
 
 	public function update($id){
 
-		$sql  = "UPDATE $this->table SET nome = :nome, email = :email, senha = :senha, endereco = :endereco, numero = :numero, bairro = :bairro, cidade = :cidade, estado = :estado, cep = :cep, nomeMae = :nomeMae, nomePai = :nomePai, numRG = :numRG, numCPF = :numCPF, dataNasc = :dataNasc, sexo = :sexo, modalidade = :modalidade WHERE id = :id";
+		$sql  = "UPDATE usuarios SET nome = :nome, email = :email, senha = :senha, endereco = :endereco, numero = :numero, bairro = :bairro, cidade = :cidade, estado = :estado, cep = :cep, nomeMae = :nomeMae, nomePai = :nomePai, numRG = :numRG, numCPF = :numCPF, dataNasc = :dataNasc, sexo = :sexo, modalidade = :modalidade WHERE id = :id";
 		$stmt = $this->prepare($sql);
 		$stmt->bindParam(':nome', $this->nome);
 		$stmt->bindParam(':email', $this->email);
