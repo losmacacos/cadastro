fdb3c3bbb1f973505c290f38a9492682b79ee077
losmacacos <alexandrferreira@gmail.com>
2017-11-12 14:44:05 -0200

    thirdth commit

--- classes/DB.php
+++ classes/DB.php
@@ -16,9 +16,9 @@ class DB{
 				$dbopts = parse_url(getenv('postgres://wolqehbjdhqouv:6f8c7b8b99faa31823c88f37df3b6bc315f2320ee9cf75a033740c4c859f5322@ec2-107-22-167-179.compute-1.amazonaws.com:5432/dbqc4gqll2o9ps'));
 				$app->register(new Herrera\Pdo\PdoServiceProvider(),
 				               array(
-				                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-				                   'pdo.username' => $dbopts["user"],
-				                   'pdo.password' => $dbopts["pass"]
+				                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["ec2-107-22-167-179.compute-1.amazonaws.com"] . ';port=' . $dbopts["5432"],
+				                   'pdo.username' => $dbopts["wolqehbjdhqouv"],
+				                   'pdo.password' => $dbopts["6f8c7b8b99faa31823c88f37df3b6bc315f2320ee9cf75a033740c4c859f5322"]
 				               )
 				);
 				$instance = $dbopts;
