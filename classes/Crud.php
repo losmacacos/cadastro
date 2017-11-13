fd766c7f3ece58494917d7f2c0c92b188fd10efc
losmacacos <alexandrferreira@gmail.com>
2017-11-13 21:41:57 -0200

    thidth-eight commit

--- classes/Crud.php
+++ classes/Crud.php
@@ -18,10 +18,24 @@ abstract class Crud extends DB{
 	}
 
 	public function findAll(){
-		$sql  = "SELECT * FROM $this->table;";
-		$stmt = DB::prepare($sql);
-		$stmt->execute();
-		return $stmt->fetchAll();
+		$dbopts = parse_url(getenv('postgres://zwjaegfjfxxizf:5049949664477c785135e52fbb54265edc71a7a3c2c67cfdcc878f16f7763741@ec2-184-73-174-10.compute-1.amazonaws.com:5432/d9vdiign21bcuv'));
+		$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+	);
+		$app->get('/db/', function() use($app) {
+  		$st = $app['pdo']->prepare('SELECT * FROM usuarios');
+  		$st->execute();
+
+		
 	}
 
 	public function delete($id){
